929ed84d0ae80bcabc5c3babb47d04b83ef026af
tleniger <95964164+TiesLeniger@users.noreply.github.com>
2025-06-22 20:56:50 +0200

    started work on fembeam

---
+++ src/fembeam.py
@@ -0,0 +1,112 @@
+import numpy as np
+import json
+from pathlib import Path
+
+class FEMBeam:
+
+    def __init__(self, filename: str):
+        # Construct path object to input file
+        path_to_file = Path.cwd() / "input" / filename
+        
+        # Check filetype
+        if not path_to_file.suffix:
+            path_to_file = path_to_file.with_suffix('.json')
+        elif path_to_file.suffix.lower() != ".json":
+            raise ValueError(f"Expected JSON file, got {path_to_file.suffix}")
+        
+        # Read json data into a dict and assign attributes to FEMBeam class
+        with open(path_to_file) as file:
+            json_dict = json.load(file)
+        
+        # Initialise FEM properties
+        fem_dict = json_dict['fem']
+        self._init_fem_properties(fem_dict)
+
+        # Initialse DMO
+        if "dmo" in json_dict and json_dict['dmo']:                     # Check if it is present and non empty
+            dmo_dict = json_dict['dmo']
+            self._init_dmo_properties(dmo_dict)
+
+    def _init_fem_properties(self, fem_dict: dict):
+        self.n_el = fem_dict['n_el']
+        self.n_nd = self.n_el + 1
+        self.n_dof = self.n_nd*3
+        self.l = fem_dict['l']
+        self.EI = fem_dict['EI']*np.ones((self.n_el,))
+        self.GJ = fem_dict['GJ']*np.ones((self.n_el,))
+        self.KbT = fem_dict['KBT']*np.ones((self.n_el,))
+        self.m = fem_dict['m']*np.ones((self.n_el,))
+        self.Ip = fem_dict['Ip']*np.ones((self.n_el,))
+        self.d = fem_dict['d']*np.ones((self.n_el,))
+        self.y_nd = np.linspace(0, self.l, self.n_nd)
+        self.L_el = np.diff(self.y_nd)
+        self.bc = fem_dict['bc']
+        b_k = np.zeros((self.n_dof,), dtype = bool)
+        if self.bc == "CF":
+            b_k[:3] = True
+        self.b_k = b_k
+        self.b_u = ~b_k
+
+    def _init_dmo_properties(self, dmo_dict: dict):
+        self.n_dmo = len(dmo_dict['ay'])
+        self.y_dmo = np.array(dmo_dict['ay'])*self.l
+        self.m_dmo = np.array(dmo_dict['m'])
+        self.d_dmo = np.array(dmo_dict['d'])
+        self.S_dmo = self.m_dmo*self.d_dmo
+        self.I_dmo = self.m_dmo*self.d_dmo**2 + np.array(dmo_dict['I0'])
+
+    def calculate_stiffness_matrix(self) -> np.ndarray:
+        """
+        Assembles the global stiffness matrix of the beam.
+
+        The organisation of the DOFs is:
+        ---------------------------------------------------
+        theta_i -> torsional deflection at node i
+        v_i     -> bending out-of plane deflection at node i
+        beta_i  -> bending rotation at node i
+        ---------------------------------------------------
+        theta_i+1 -> torsional deflection at node i+1
+        v_i+1     -> bending out-of plane deflection at node i+1
+        beta_i+1  -> bending rotation at node i+1
+        ---------------------------------------------------
+
+        :param fem: dictionary with discretised FE model parameters
+        :return KK: global stiffness matrix:
+        """
+        def _calculate_element_stiffness_matrix(l: float, EI: float, GJ: float, KbT: float) -> np.ndarray:
+            mat_K = np.zeros((6, 6))
+
+            mat_K[0, 0] = GJ/l
+            mat_K[0, 2] = mat_K[2, 0] = KbT/l
+            mat_K[0, 3] = mat_K[3, 0] = -mat_K[0, 0]
+            mat_K[0, 5] = mat_K[5, 0] = -mat_K[0, 2]
+
+            mat_K[1, 1] = 12.0*EI/l**3
+            mat_K[1, 2] = mat_K[2, 1] = 6.0*EI/l**2
+            mat_K[1, 4] = mat_K[4, 1] = -mat_K[1, 1]
+            mat_K[1, 5] = mat_K[5, 1] = mat_K[1, 2]
+
+            mat_K[2, 2] = 4.0*EI/l
+            mat_K[2, 3] = mat_K[3, 2] = -mat_K[0, 2]
+            mat_K[2, 4] = mat_K[4, 2] = -mat_K[1, 2]
+            mat_K[2, 5] = mat_K[5, 2] = 2*EI/l
+
+            mat_K[3, 3] = mat_K[0, 0]
+            mat_K[3, 5] = mat_K[5, 3] = mat_K[0, 2]
+
+            mat_K[4, 4] = mat_K[1, 1]
+            mat_K[4, 5] = mat_K[5, 4] = -mat_K[1, 2]
+
+            mat_K[5, 5] = mat_K[2, 2]
+
+            return mat_K
+
+        # Create global mass matrix:
+        KK = np.zeros((self.n_dof, self.n_dof))
+        for i in range(self.n_el):
+            KK[3*i:3*(i + 2), 3*i:3*(i + 2)] += _calculate_element_stiffness_matrix(self.L[i], self.EI[i], self.GJ[i], self.KBT[i])
+
+        # Apply boundary conditions:
+        self.KK_red = KK[self.b_u, :][:, self.b_u]
+
+        

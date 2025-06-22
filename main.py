c407328d960b404f21c8afe866b9469ea40871c9
tleniger <95964164+TiesLeniger@users.noreply.github.com>
2025-06-22 14:37:09 +0200

    main file for running

--- main.py
+++ main.py
@@ -3,6 +3,21 @@ from src.vortex import *
 from src.solver import *
 from src.postprocessing import postprocessing
 
-wing = TangDowellWing(6, 18, "constant", "cosine")
+D2R = np.pi/180
+R2D = 180/np.pi
 
-wing.plot_wing()
+# Instantiate wing
+wing = TangDowellWing(4, 8, "constant", "cosine")
+
+# Define operating conditions
+rho = 1.225                         # [kg/m^3], air density
+v_inf = 20.0                        # [m/s], free stream velocity
+alpha = 5.0 * D2R                   # [deg -> rad], angle of attack
+l_wake_c = 10*wing.chord            # [m], wake length w.r.t chord length
+
+# Generate solutions
+results = generate_solutions(wing, v_inf, alpha, l_wake_c)
+
+# Postprocess
+C_L, C_D = postprocessing(results, wing, rho, v_inf, make_plots = True)
+print(C_L, C_D)
